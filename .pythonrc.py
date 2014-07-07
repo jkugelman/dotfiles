8e806860e440a890b1f4ef661e348ddb8f902106
John Kugelman <john@kugelman.name>
2014-07-07 18:15:18 -0400

    Add Python interactive readline support

---
+++ .pythonrc.py
@@ -0,0 +1,25 @@
+# Add auto-completion and a stored history file of commands to your Python
+# interactive interpreter. Requires Python 2.0+, readline. Autocomplete is
+# bound to the Esc key by default (you can change it - see readline docs).
+#
+# Store the file in ~/.pystartup, and set an environment variable to point
+# to it:  "export PYTHONSTARTUP=~/.pystartup" in bash.
+
+import atexit
+import os
+import readline
+import rlcompleter
+
+history_path = os.path.expanduser("~/.pyhistory")
+
+def save_history(history_path=history_path):
+    import readline
+    readline.write_history_file(history_path)
+
+if os.path.exists(history_path):
+    readline.read_history_file(history_path)
+
+readline.parse_and_bind('tab: complete')
+atexit.register(save_history)
+
+del os, atexit, readline, rlcompleter, save_history, history_path
