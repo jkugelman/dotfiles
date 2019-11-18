208307ccfac8064f64e5326c3edc66246ad7ebe4
John Kugelman <john.kugelman@progeny.net>
2019-11-17 21:21:20 -0500

    Add commented out angled  prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -196,6 +196,25 @@
   # Left prompt terminator for lines without any segments.
   typeset -g POWERLEVEL9K_EMPTY_LINE_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL=
 
+  # # Separator between same-color segments on the left.
+  # typeset -g POWERLEVEL9K_LEFT_SUBSEGMENT_SEPARATOR=''
+  # # Separator between same-color segments on the right.
+  # typeset -g POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR=''
+  # # Separator between different-color segments on the left.
+  # typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR=''
+  # # Separator between different-color segments on the right.
+  # typeset -g POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR=''
+  # # The right end of left prompt.
+  # typeset -g POWERLEVEL9K_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL=''
+  # # The left end of right prompt.
+  # typeset -g POWERLEVEL9K_RIGHT_PROMPT_FIRST_SEGMENT_START_SYMBOL=''
+  # # The left end of left prompt.
+  # typeset -g POWERLEVEL9K_LEFT_PROMPT_FIRST_SEGMENT_START_SYMBOL=''
+  # # The right end of right prompt.
+  # typeset -g POWERLEVEL9K_RIGHT_PROMPT_LAST_SEGMENT_END_SYMBOL=''
+  # # Left prompt terminator for lines without any segments.
+  # typeset -g POWERLEVEL9K_EMPTY_LINE_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL=
+
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=145
